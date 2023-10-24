d583a7887c992e85749ff5ceeafb16a705e75af4
adrielle-b <adriielle@gmail.com>
2023-10-24 16:59:38 -0300

    feat05: encontra numero repetido

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,10 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    num_repet = set()
+    for num in nums:
+        if not isinstance(num, int) or num < 0:
+            return False
+        if num in num_repet:
+            return num
+        num_repet.add(num)
+    return False
+
