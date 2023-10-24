4384bd8b51f305d05153bcb4b36c0aa7730d9304
adrielle-b <adriielle@gmail.com>
2023-10-24 17:11:52 -0300

    feat06: palindromo com iteratividade

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,12 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    n = len(word)
+
+    for index in range(n):
+        if word[index] != word[n - index - 1]:
+            return False
+
+    return True
+
