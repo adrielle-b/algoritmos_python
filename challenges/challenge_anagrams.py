4d8d3bafe72f6ce3c8698410371783d619e17d94
adrielle-b <adriielle@gmail.com>
2023-10-24 16:29:56 -0300

    feat04: anagramas com ordenacao

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,44 @@
+def merge_sort(string, start=0, end=None):
+    if end is None:
+        end = len(string)
+    if (end - start) > 1:
+        mid = (start + end) // 2
+        merge_sort(string, start, mid)
+        merge_sort(string, mid, end)
+        merge(string, start, mid, end)
+    return string
+
+
+def merge(string, start, mid, end):
+    left = string[start:mid]
+    right = string[mid:end]
+
+    left_index, right_index = 0, 0
+
+    for general_index in range(start, end): 
+        if left_index >= len(left):
+            string[general_index] = right[right_index]
+            right_index = right_index + 1
+        elif right_index >= len(right):
+            string[general_index] = left[left_index]
+            left_index = left_index + 1
+        elif left[left_index] < right[right_index]:
+            string[general_index] = left[left_index]
+            left_index = left_index + 1
+        else:
+            string[general_index] = right[right_index]
+            right_index = right_index + 1
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_string = merge_sort(list(first_string.lower()))
+    second_string = merge_sort(list(second_string.lower()))
+
+    first_string = ''.join(first_string)
+    second_string = ''.join(second_string)
+
+    if not first_string or not second_string:
+        return (first_string, second_string, False)
+    else:
+        anagram_verify = first_string == second_string
+        return (first_string, second_string, anagram_verify)
