350b8c644cac255b13e10f05b8a27fa42e26b7b6
adrielle-b <adriielle@gmail.com>
2023-10-24 15:30:31 -0300

    feat02: teste da criptografia

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
-
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("senha", 2) == "ahn_es"
+    assert encrypt_message("senha", 3) == "nes_ah"
+    assert encrypt_message("senha", 9) == "ahnes"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("senha", "s")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1, 1)
